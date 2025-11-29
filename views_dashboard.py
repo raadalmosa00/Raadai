a2f1b0aaaaf6c4115d97e50e159ea39e515950ca
raadalmosa00 <raadalmosa00@gmail.com>
2025-11-29 17:06:47 -0500

    Delete views_dashboard.py

--- views_dashboard.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, "index_home.html")
-
-def dashboard_home(request):
-    return render(request, "dashboard_index.html")
