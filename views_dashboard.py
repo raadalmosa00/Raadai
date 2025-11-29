5c4f56fd5c60cf76ec3b3b70ff2927b38085ca73
raadalmosa00 <raadalmosa00@gmail.com>
2025-11-29 16:13:32 -0500

    Delete views_dashboard.py

--- views_dashboard.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def dashboard_home(request):
-    return render(request, "dashboard_index.html")
